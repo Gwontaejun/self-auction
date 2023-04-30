6c73b83c17de3415e6d93bd728a12117a4be0a74
권태준 <44458173+Gwontaejun@users.noreply.github.com>
2023-04-30 19:26:57 +0900

    Feat: 프로젝트 구성
    :Detailed Notes:
    - ESLint (airbnb)
    - Prettier
    - Typescript
    - Nextjs (SSG 테스트 페이지 작성)

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface PropType {
+  posts: string;
+}
+
+const test = (props: PropType) => {
+  return <div>{props.posts}</div>;
+};
+
+export async function getStaticProps() {
+  const posts = '2';
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  };
+}
+
+export default test;
